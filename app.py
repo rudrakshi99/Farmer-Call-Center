2496db2e5a864978c3b24f1e163d9dd027aee830
rudrakshi99 <sonirudrakshi99@gmail.com>
2022-11-08 23:22:18 +0530

    added response payload

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_mysqldb import MySQL
 from twilio.rest import Client
 
-import utils
+from utils import response_payload
 
 from config import config
 
@@ -11,59 +11,57 @@ app = Flask(__name__)
 conexion = MySQL(app)
 
 
-#The thelephone number associated whit the twilio account is: 
+# The thelephone number associated whit the twilio account is:
 # --------> +240222139113
 # The titles of the topics stored in the database are:
-    # Nutrition
-    # Fertilizers
-    # Cultivation
-    # Harvest
+# Nutrition
+# Fertilizers
+# Cultivation
+# Harvest
 
-    
 
-@app.route('/find_response/<phone_number>/<message_body>', methods=['GET','POST'])
-def find_response(phone_number,message_body):
+@app.route("/find_response/<phone_number>/<message_body>", methods=["GET", "POST"])
+def find_response(phone_number, message_body):
     try:
-        cursor=conexion.connection.cursor()
-        sql="SELECT content FROM topics WHERE topic_title = '{0}'".format(message_body)
+        cursor = conexion.connection.cursor()
+        sql = "SELECT content FROM topics WHERE topic_title = '{0}'".format(
+            message_body
+        )
         cursor.execute(sql)
-        data=cursor.fetchone()
+        data = cursor.fetchone()
         if data != None:
             response = data
-            
-            
-            def sms_response(phone_number,response):
+
+            def sms_response(phone_number, response):
                 account_sid = "AC06e69898844aa7fb0f8790830b8fde17"
                 auth_token = "352f7b01b9d0aa47b3276114f8a13f60"
 
-                
                 client = Client(account_sid, auth_token)
                 message = client.messages.create(
-                    body = response,
-                    from_ = "+18159576078", 
-                    to = phone_number
+                    body=response, from_="+18159576078", to=phone_number
                 )
+
             try:
                 sms_response(phone_number, response)
             except Exception as ex:
-                return jsonify({'message':"Check your internet conection."})
-            
-            return jsonify({'topic':response, 'message':"Topic found."})
-        else:    
-            return jsonify({'message':"Topic not found."})
+                return response_payload(False, msg="Check your internet connection.")
+
+            return response_payload(
+                True, data=response, msg="Message sent successfully."
+            )
+        else:
+            return response_payload(
+                False, msg="The topic you are looking for is not in the database."
+            )
     except Exception as ex:
-        return jsonify({'message':"Error"})
-    
+        return response_payload(False, msg="Check your internet connection.")
 
 
 def page_not_found(error):
     return "<h1> Page not found ...", 404
-      
 
-if __name__ == '__main__':
-    app.config.from_object(config['development'])
+
+if __name__ == "__main__":
+    app.config.from_object(config["development"])
     app.register_error_handler(404, page_not_found)
     app.run()
-    
-    
-    
