b00c5b9cc707e22d6e02e28166e9d0b892d0de1d
paondonchama <78755386+paondonchama@users.noreply.github.com>
2022-11-08 11:26:51 +0100

    Completed the Api Rest, and the sms function too

---
+++ config.py
@@ -0,0 +1,19 @@
+class DevelopmentConfig():
+    DEBUG = True
+    
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'geeks_on_fire'
+    MYSQL_HOST = 'localhost'
+    
+config = {
+    'development' : DevelopmentConfig
+}
+
+account_sid = "AC06e69898844aa7fb0f8790830b8fde17"
+auth_token = "352f7b01b9d0aa47b3276114f8a13f60"
+system_phone = "+18159576078"
+
+    
+    
+    
