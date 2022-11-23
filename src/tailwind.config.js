e1d72723a63d53bb6dcd46dafce62008bb6976dd
rudrakshi99 <sonirudrakshi99@gmail.com>
2022-11-23 12:56:07 +0530

    initial setup

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                'evergreen': '#D8F5DE',
+                'darkgreen': '#68AC5D'
+            },
+        },
+    },
+    plugins: [],
+};
