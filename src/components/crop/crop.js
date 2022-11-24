501dfeaf95dcf3d263a682473d4eac03a4b5bf65
faremi <saheedfaremi@gmail.com>
2022-11-23 20:09:23 -0500

    add: crop and fertilizer api

--- src/components/crop/crop.js
+++ src/components/crop/crop.js
@@ -1,9 +1,56 @@
 import PreHeader from "../preheader/preheader";
 import Header from "../Header/Header";
 import Footer from "../Footer/footer";
-import React from "react";
+import React, { useState } from "react";
 
 const Crop = () => {
+
+  const [load, setLoad] = useState(false);
+  const [nitrogen, setNitrogen] = useState("");
+  const [phosphorus, setPhosphorus] = useState("");
+  const [potassium, setPotassium] = useState("");
+  const [ph, setPh] = useState("");
+  const [rain, setRain] = useState("");
+  const [city, setCity] = useState("");
+  const [prediction, setPrediction] = useState("");
+
+  function onSearchSubmit(term) {
+    setLoad(true);
+    console.log("Clicked");
+    let url = "http://127.0.0.1:5000/crop-recommedation";
+    let body = JSON.stringify({
+      "nitrogen": parseFloat (nitrogen),
+      "phosphorous": parseFloat (phosphorus),
+      "pottasium": parseFloat (potassium),
+      "ph": parseFloat (ph),
+      "rainfall": parseFloat (rain),
+      "city": city,
+    });
+    console.log("body", body);
+    try{
+      fetch(url, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: body
+
+      }).then(response => response.json())
+        .then(data => {
+          let main_data = data["data"];
+          setPrediction(main_data['prediction']);
+          console.log('res',data); // gives SyntaxError: Unexpected end of input
+        }).catch((error) => {
+        console.log(error)
+      });
+    }catch (e) {
+      console.log(e)
+    }
+
+    setLoad(false);
+  }
+
   return (
     <>
       <PreHeader />
@@ -13,16 +60,53 @@ const Crop = () => {
           <div className="container bg-gray-100 p-10 grid place-items-center mt-14  ">
             <p className="text-2xl font-medium text-green-600 my-12">Predict the best crop to plant<br /></p>
 
-            <input className="w-3/5 my-2 " type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
+            <input onChange={
+              (e) => {
+                setNitrogen(e.target.value);
+              }
+            } className="w-3/5 my-2 " type="text" placeholder="Enter the value of nitrogen" />
+            <input
+              onChange={
+                (e) => {
+                  setPhosphorus(e.target.value);
+                }
+              }
+              className="w-3/5 my-2" type="text" placeholder="Enter the value of Phosphorus" />
+            <input
+              onChange={
+                (e) => {
+                  setPotassium(e.target.value);
+                }
+              }
+              className="w-3/5 my-2" type="text" placeholder="Enter the value of Potassium" />
+            <input
+              onChange={
+                (e) => {
+                  setPh(e.target.value);
+                }
+              }
+              className="w-3/5 my-2" type="text" placeholder="Enter the soil ph" />
+            <input
+              onChange={
+                (e) => {
+                  setRain(e.target.value);
+                }
+              }className="w-3/5 my-2" type="text" placeholder="Enter the rainfall gauge" />
+            <input
+              onChange={
+                (e) => {
+                  setCity(e.target.value);
+                }
+              }
+              className="w-3/5 my-2" type="text" placeholder="Enter your city" />
 
             <div className="grid place-items-center mt-14 ">
               <div className="mt-2">
-                <button type="button"
+                <button onClick={
+                  () => {
+                    onSearchSubmit();
+                  }
+                } type="button"
                         className="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Get
                  Crop Recommendation
                 </button>
@@ -32,6 +116,13 @@ const Crop = () => {
           </div>
 
 
+        </div>
+
+        <div>
+          {load ? <div className="grid place-items-center my-14  ">loading </div> : <div></div>}
+          {prediction !== '' ? <div className="grid place-items-center my-14 text-center ">
+            <p className="font-bold my-3">Crop Predicted: </p>{prediction}
+          </div> : <div></div>}
         </div>
       </section>
       <Footer />
--- src/components/fertilizer/fertilizer.js
+++ src/components/fertilizer/fertilizer.js
@@ -1,10 +1,67 @@
 import PreHeader from "../preheader/preheader";
 import Header from "../Header/Header";
 import Footer from "../Footer/footer";
-import React from "react";
+import React, { useState } from "react";
 
 const Fertilizer = () => {
-  return (
+  const [load, setLoad] = useState(false);
+  const [soilType, setSoilType] = useState("");
+  const [cropType, setCropType] = useState("");
+  const [moisture, setMoisture] = useState("");
+  const [nitrogen, setNitrogen] = useState("");
+  const [phosphorus, setPhosphorus] = useState("");
+  const [potassium, setPotassium] = useState("");
+  const [city, setCity] = useState("");
+  const [prediction, setPrediction] = useState("");
+  const [information, setInformation] = useState("");
+  const [application, setApplication] = useState("");
+  const [specification, setSpecification] = useState("");
+ function onSearchSubmit(term) {
+    setLoad(true);
+    console.log("Clicked");
+    let url = "http://127.0.0.1:5000/fertilizer-predict";
+    let body = JSON.stringify({
+      "soil-type": soilType,
+      "crop-type": cropType,
+      "moisture": parseFloat (moisture),
+      "nitrogen": parseFloat (nitrogen),
+      "phosphorous": parseFloat (phosphorus),
+      "pottasium": parseFloat (potassium),
+      "city": city,
+    });
+    console.log("body", body);
+    try{
+       fetch(url, {
+        // mode: "no-cors",
+        method: "post",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: body
+
+      }).then(response => response.json())
+        .then(data => {
+          let main_data = data["data"];
+          setPrediction(main_data['prediction']);
+          setInformation(main_data['info']['info']);
+          setApplication(main_data['info']['application']["1"]+ " " + main_data['info']['application']["2"]);
+          setSpecification(main_data['info']['specifications']);
+          console.log('res',data); // gives SyntaxError: Unexpected end of input
+        }).catch((error) => {
+          console.log(error)
+        });
+    }catch (e) {
+      console.log(e)
+    }
+
+   setLoad(false);
+
+
+
+  }
+
+  return(
     <>
       <PreHeader />
       <Header />
@@ -13,16 +70,19 @@ const Fertilizer = () => {
           <div className="container bg-gray-100 p-10 grid place-items-center mt-14  ">
             <p className="text-2xl font-medium text-green-600 my-12">Predict the Fertilizer for your crop<br /></p>
 
-            <input className="w-3/5 my-2 " type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
-            <input className="w-3/5 my-2" type="text" placeholder="Search" />
+            <input onChange={(e)=> setSoilType(e.target.value)} className="w-3/5 my-2 " type="text" placeholder="Enter soil type" />
+            <input onChange={(e)=> setCropType(e.target.value)} className="w-3/5 my-2" type="text" placeholder="Enter crop type" />
+            <input onChange={(e)=> setMoisture(e.target.value)} className="w-3/5 my-2" type="text" placeholder="Enter moisture value" />
+            <input onChange={(e)=> setNitrogen(e.target.value)} className="w-3/5 my-2" type="text" placeholder="Enter nitrogen value" />
+            <input onChange={(e)=> setPhosphorus(e.target.value)} className="w-3/5 my-2" type="text" placeholder="Enter phosphorous value" />
+            <input onChange={(e)=> setPotassium(e.target.value)} className="w-3/5 my-2" type="text" placeholder="Enter potassium value" />
+            <input onChange={(e)=> setCity(e.target.value)} className="w-3/5 my-2" type="text" placeholder="Enter city" />
 
             <div className="grid place-items-center mt-14 ">
               <div className="mt-2">
-                <button type="button"
+                <button onClick={
+               () =>  onSearchSubmit("aaa")
+                } type="button"
                         className="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Get
                   Fertilizer Recommendation
                 </button>
@@ -32,6 +92,15 @@ const Fertilizer = () => {
           </div>
 
 
+        </div>
+        <div>
+          {load ? <div className="grid place-items-center my-14  ">loading </div> : <div></div>}
+          {prediction !== '' ? <div className="grid place-items-center my-14 text-center ">
+          <p className="font-bold my-3">Fertilizer Predicted: </p>{prediction}
+            <p className="font-bold my-3">Information</p>{information}
+            <p className="font-bold my-3">Application</p>{application}
+            <p className="font-bold my-3">Specification</p>{specification}
+          </div> : <div></div>}
         </div>
       </section>
       <Footer />
